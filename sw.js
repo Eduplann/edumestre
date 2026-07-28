2b6e8a0152762ad8bd5f42c334ce73576f36c239
Eduplann <edumestre2026@gmail.com>
2026-07-28 11:54:04 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,36 @@
-const CACHE_NAME = 'edumestre-v1';
-const assets = ['./index.html', './manifest.json'];
+const CACHE_NAME = 'edumestre-glow-cache-v2';
+const ASSETS = [
+  './',
+  './index.html?v=2',
+  './manifest.json?v=2'
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    }).then(() => self.skipWaiting())
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key); // Deleta versões velhas automaticamente
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => {
+      return caches.match(e.request);
+    })
+  );
 });
